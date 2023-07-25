b50819f68f2ee252f17f4e5cba2fddd15514aa47
mosesnyota2015 <mosesnyota2015@gmail.com>
2023-07-25 14:01:35 -0700

    Added helper function to get appropriate function

--- custom_printf.c
+++ custom_printf.c
@@ -49,3 +49,61 @@ int _printf(const char *format, ...)
 	va_end(args);
 	return (cnt);
 }
+
+
+/**
+ * get_base_len - return the length of the base
+ * @n: int
+ * @b: int
+ * Return: int
+ */
+
+int get_base_len(int n, int b)
+{
+	int len = 0;
+
+	while (n > 0)
+	{
+		n /= b;
+		len++;
+	}
+
+	return (len);
+}
+
+/**
+ * get_func - returns the correct func or null
+ * @id: char
+ * Return: function
+ */
+
+int (*get_func(const char id))(va_list args)
+{
+	prv_t prv[] = {
+		{'c', print_char},
+		{'s', print_str},
+		{'b', print_bin},
+		{'d', print_dec},
+		{'i', print_dec},
+		{'u', print_un_int},
+		{'o', print_octa},
+		{'x', print_hex},
+		{'X', print_heX},
+		{'S', print_non_prt},
+		{'r', print_rev},
+		{'R', print_rot},
+		{'p', print_ptr}
+	};
+
+	const int PRV_LEN = 13;
+	int i = 0;
+
+	while (i < PRV_LEN)
+	{
+		if (id == prv[i].id)
+			return (prv[i].f);
+		i++;
+	}
+
+	return (NULL);
+}
